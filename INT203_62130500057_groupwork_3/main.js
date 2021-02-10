a62c77c221920703e53178988d73d13abfc677e6
gun082544 <gun082544@hotmail.com>
2021-02-10 10:46:36 +0700

    [ADD] groupwork_3

---
+++ INT203_62130500057_groupwork_3/main.js
@@ -0,0 +1,23 @@
+const app = {
+    data() {
+        return {
+            pictures: [
+                { path: './images/1.jpeg', description: 'test1' ,showFav: false },
+                { path: './images/2.jpeg', description: 'test2' ,showFav: false},
+                { path: './images/3.jpeg', description: 'test3' ,showFav: false}
+            ],
+            path:"./images/1.jpeg"
+        }
+    },
+    methods:{
+        showFavIcon(index){
+            this.pictures[index].showFav = !this.pictures[index].showFav;
+        }
+    },
+    computed:{
+        countShow(){
+            return this.pictures.filter(s => s.showFav).length;
+        }
+    }
+};
+Vue.createApp(app).mount('#app');
